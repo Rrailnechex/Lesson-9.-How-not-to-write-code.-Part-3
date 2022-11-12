8e1869601b51d843c6ac28fdd047c892c771dd9b
Ygor <wanerosu8@gmail.com .com>
2022-11-12 23:31:14 +0300

    first commit

---
+++ Задача 64/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+/* 
+Задайте значение N. Напишите программу, которая 
+выведет все чётные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+
+N = 5 -> "4, 2"
+N = 8 -> "8, 6, 4, 2,"
+
+ */
+
+
+int SolveTasc64(int num)
+{
+    if (i >= num) return 0;
+    else
+        Console.WriteLine(num);
+    return SolveTasc64(i + 1);
+}
+
+
+Console.Write("num: ");
+int num = int.Parse(Console.ReadLine());
+
+SolveTasc64(num);
---
+++ Задача 66/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+/* 
+Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов 
+в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+ */
+
+//int[] theArray = new int[];
+
+int SolveTask66(int m, int n)
+{
+    return m == n - 1 ? m : SolveTask66(m + 1, n) + m;
+}
+
+
+
+Console.Write("m: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("n: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Сумма наткральных элементов: {SolveTask66(m + 1, n)}");
