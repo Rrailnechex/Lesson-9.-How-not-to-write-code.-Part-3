b93e78451ade7e279b6d57eb72ce1c3c4e6f5b37
Ygor <wanerosu8@gmail.com .com>
2022-11-13 12:46:38 +0300

    64 solved

--- Задача 64/Program.cs
+++ Задача 64/Program.cs
@@ -9,18 +9,19 @@ N = 5 -> "4, 2"
 N = 8 -> "8, 6, 4, 2,"
 
  */
-// f z tve ujdjh.
 
-int SolveTasc64(int num)
+int SolveTasc64(int num, int i)
 {
-    if (i >= num) return 0;
+    if (i == num) return 0;
     else
-        Console.WriteLine(num);
-    return SolveTasc64(i + 1);
+        if (num % 2 == 0) Console.Write($"{num}, ");
+    return SolveTasc64(num - 1, i);
 }
 
 
 Console.Write("num: ");
 int num = int.Parse(Console.ReadLine());
 
-SolveTasc64(num);
+Console.Write($"'");
+SolveTasc64(num, 1);
+Console.Write($"'");
